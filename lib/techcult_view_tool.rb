bab684e40722cd4d12edea3aa9854c36c8434d29
deva1156 <rgs264rgs@gmail.com>
2020-02-29 12:44:17 +0530

    Techcult intial commit

---
+++ lib/techcult_view_tool.rb
@@ -0,0 +1,7 @@
+require "techcult_view_tool/version"
+require "techcult_view_tool/renderer"
+
+module TechcultViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/techcult_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TechcultViewTool
+  VERSION = "0.1.0"
+end
